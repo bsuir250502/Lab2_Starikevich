446ddafcd064ad4aed09f893dd44ace5b2e5daba
Max Starikevich <max@hybrid.(none)>
2013-05-21 16:56:09 +0300

    fixed mistakes

--- mylib.c
+++ mylib.c
@@ -9,57 +9,57 @@ extern "C" {
 
 #define string 64
 
-    int print_manual() {
+int print_manual() {
 	char buffer[string];
 	FILE *fp = fopen("README.md", "r");
 	while (fgets(buffer, string, fp)) {
-	    printf("%s", buffer);
+		printf("%s", buffer);
 	} printf("\n");
 	fclose(fp);
 
 	return 0;
-    }
+}
 
-    char myget_str(char *str, int length) {
-	fgets(str, length, stdin);
-	str[strlen(str) - 1] = '\0';
+char myget_str(char *str, int length) {
+fgets(str, length, stdin);
+str[strlen(str) - 1] = '\0';
 
-	return *str;
-    }
+return *str;
+}
 
-    int myget_int() {
+int myget_int() {
 	int number;
 
 	char buffer[string];
 	while (1) {
-	    fgets(buffer, string, stdin);
-	    number = atoi(buffer);
-	    if (number <= 0) {
+		fgets(buffer, string, stdin);
+		number = atoi(buffer);
+		if (number <= 0) {
 		printf("Error! Please, enter the correct number...: ");
 		continue;
-	    }
-	    break;
+		}
+		break;
 	}
 
 	return number;
-    }
+}
 
-    int myget_int_range(int a, int b) {
+int myget_int_range(int a, int b) {
 	int number;
 	while (1) {
-	    number = myget_int();
+		number = myget_int();
 
-	    if (number >= a && number <= b) {
+		if (number >= a && number <= b) {
 		return number;
 		break;
-	    } else {
+		} else {
 		printf
-		    ("Error! Please, enter the correct number in range [%d-%d]...: ",
-		     a, b);
+			("Error! Please, enter the correct number in range [%d-%d]...: ",
+			 a, b);
 		continue;
-	    }
+		}
 	}
-    }
+}
 
 #ifdef __cplusplus
 }
